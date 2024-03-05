e9ab02b7ec38179edc1947e932979f1f48ab0f70
thomasclarito <43707071+thomasclarito@users.noreply.github.com>
2024-03-05 01:39:41 -0800

    Replace legacy HandLandmarker with new HandLandmarker

--- HandDetector.py
+++ HandDetector.py
@@ -1,17 +1,33 @@
 import cv2
 import mediapipe as mp
+from mediapipe.framework.formats import landmark_pb2
+import os
 
 from constants import WINDOW_HEIGHT
 from constants import WINDOW_WIDTH
 from Cursor import Cursor
 
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+model_path = os.path.abspath("./models/hand_landmarker.task")
+
 
 class HandDetector:
     def __init__(self):
+        options = HandLandmarkerOptions(
+            base_options=BaseOptions(model_asset_path=model_path),
+            running_mode=VisionRunningMode.IMAGE,
+            num_hands=1,
+        )
+
         # Create a VideoCapture object to access the webcam
         self.cap = cv2.VideoCapture(0)
         # Create a hand tracking module
-        self.mp_hands = mp.solutions.hands.Hands(max_num_hands=1)
+        # self.mp_hands = mp.solutions.hands.Hands(max_num_hands=1)
+        self.landmarker = HandLandmarker.create_from_options(options)
 
     def get_pointer_location(self, cursor: Cursor) -> Cursor:
         """Get the location of the index finger tip."""
@@ -25,28 +41,28 @@ class HandDetector:
         # Convert the frame to RGB
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # Process the frame with the hand tracking module
-        results = self.mp_hands.process(frame_rgb)
-
-        # Check if any hands are detected
-        if results.multi_hand_landmarks:
-            # handedness = results.multi_handedness[0]
-            # classification = handedness.classification[0]
-            # if classification.label == "Right":
-            hand_landmarks = results.multi_hand_landmarks[0]
-            index_tip = hand_landmarks.landmark[
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
+        result = self.landmarker.detect(mp_image)
+        # Detect the frame with the hand tracking module
+        if result.hand_landmarks:
+            index_tip = result.hand_landmarks[0][
                 mp.solutions.hands.HandLandmark.INDEX_FINGER_TIP
             ]
-            # Draw the hand landmarks on the frame
-            # mp.solutions.drawing_utils.draw_landmarks(frame, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS)
-            # Display the frame with the hand landmarks
+            # hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+            # hand_landmarks_proto.landmark.extend([
+            # landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in result.hand_landmarks[0]
+            # ])
+            # # Draw the hand landmarks on the frame
+            # mp.solutions.drawing_utils.draw_landmarks(frame, hand_landmarks_proto, mp.solutions.hands.HAND_CONNECTIONS)
+
             cursor.posx = int(index_tip.x * WINDOW_WIDTH)
             cursor.posy = int(index_tip.y * WINDOW_HEIGHT)
-            # print(int(index_tip.x * 800), int(index_tip.y * 600))
 
-        return cursor
+        
         # cv2.imshow("Hand Tracking", frame)
         # cv2.waitKey(1)
+        return cursor
+
 
     def cleanup(self):
         # Release the VideoCapture object
@@ -55,7 +71,7 @@ class HandDetector:
 
 # TODO: Create a new cursor class that tracks the index finger tip and displays it on the screen
 
-# # Create an instance of the HandDetector class and run it
+# Create an instance of the HandDetector class and run it
 # hand_detector = HandDetector()
 # while True:
 #     hand_detector.get_pointer_location()
